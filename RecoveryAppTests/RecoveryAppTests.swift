76b90be672b9d37df92172d89a5f02775eedb4a4
cartercoughlin <125041672+cartercoughlin@users.noreply.github.com>
2025-11-22 10:25:46 -0500

    Initial Commit

---
+++ RecoveryAppTests/RecoveryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecoveryAppTests.swift
+//  RecoveryAppTests
+//
+//  Created by Carter Coughlin on 11/22/25.
+//
+
+import Testing
+@testable import RecoveryApp
+
+struct RecoveryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
